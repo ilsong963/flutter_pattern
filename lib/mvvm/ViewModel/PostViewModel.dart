98038451368a061f3d49f2d7afd282789a889c6c
ilsong963 <ilsong963@naver.com>
2021-01-23 16:40:10 +0900

    초기

---
+++ lib/mvvm/ViewModel/PostViewModel.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:flutter_pattern/mvvm/Model/Post.dart';
+import 'package:http/http.dart' as http;
+
+
+class PostViewModel {
+  String url = 'https://jsonplaceholder.typicode.com/posts';
+
+  Future<List<Post>> fetchPost() async {
+    final response = await http.get(this.url);
+    if (response.statusCode == 200) {
+      // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
+      List responseJson = json.decode(response.body);
+      return responseJson.map((post) => new Post.fromJson(post)).toList();
+    } else {
+      // 만약 응답이 OK가 아니면, 에러를 던집니다.
+      throw Exception('Failed to load post');
+    }
+  }
+}
---
+++ lib/mvvm/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pattern/mvvm/Model/Post.dart';
+import 'package:flutter_pattern/mvvm/ViewModel/PostViewModel.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Fetch Data Example'),
+          ),
+          body: ListContainer())));
+}
+
+class ListContainer extends StatefulWidget {
+  @override
+  _ListContainerState createState() => _ListContainerState();
+}
+
+class _ListContainerState extends State<ListContainer> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            child: ListWidget()));
+  }
+}
+
+class ListWidget extends StatelessWidget {
+
+  final PostViewModel viewModel = PostViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+      future: this.viewModel.fetchPost(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('${snapshot.data[index].title}'),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // 기본적으로 로딩 Spinner를 보여줍니다.
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
